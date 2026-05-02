5ea965cfb1c77de5a64b2f03bcff5e6be560724c
AleeeGC <183964055+AleeeGC@users.noreply.github.com>
2026-05-02 12:37:10 -0600

    MapFlip nearby places

--- MapFlip/MapFlip/EditView.swift
+++ MapFlip/MapFlip/EditView.swift
@@ -16,12 +16,32 @@ struct EditView: View {
     @State private var name : String
     @State private var description : String
     
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
                 Section {
                     TextField("Name", text: $name)
                     TextField("Description", text: $description)
+                }
+                
+                Section("Nearby Places") {
+                    switch loadingState {
+                    case .loading:
+                        LoadingView()
+                    case .loaded:
+                        ForEach(pages, id:\.pageid) { page in
+                            Text(page.title).font(.headline) + Text(": ") + Text(page.description).italic()
+                        }
+                    case .failed:
+                        Text("Flopped")
+                    }
                 }
             }
             .navigationTitle("Edit Pin")
@@ -35,6 +55,29 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await findNearby()
+            }
+        }
+    }
+    
+    func findNearby() async {
+        guard let url = URL(string: "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let items = try JSONDecoder().decode(Result.self, from: data)
+                        
+            pages = items.query.pages.values.sorted()
+            
+            loadingState = .loaded
+        } catch {
+            print("Error: \(error.localizedDescription)")
+            loadingState = .failed
         }
     }
     
@@ -43,7 +86,6 @@ struct EditView: View {
         self.location = location
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
-        
     }
 }
 
